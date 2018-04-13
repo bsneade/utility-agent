cb7d09dbf69e89a91a0c30ce37c9d4b35c3ba454
Brad Sneade <bsneade@gmail.com>
2018-04-12 21:31:02 -0400

    Adding additional logging

--- src/HighestScoringSelector.ts
+++ src/HighestScoringSelector.ts
@@ -8,7 +8,7 @@ export class HighestScoringSelector implements Selector {
         const qualifierPromises = qualifiers.map(qualifier => {
             // TODO - something seems wrong here, but it seems to work
             return qualifier.score(context).then(value => {
-                console.log(`${qualifier.action.name} scored as ${value}`);
+                console.log(`HighestScoringSelector::${qualifier.action.name} scored as ${value}`);
                 return { qualifier: qualifier, value: value };
             });
         });
