ce6733051fde57268e90a51cd510c4c0b06b7ed3
Brad Sneade <bsneade@gmail.com>
2018-04-12 21:34:42 -0400

    Adding additional logging

--- src/SumOfChildrenQualifier.ts
+++ src/SumOfChildrenQualifier.ts
@@ -13,8 +13,11 @@ export class SumOfChildrenQualifier extends Qualifier {
         // loop through the scorers and sum them up
         return Promise.resolve(
             this.scorers
-                .map(scorer => scorer.score(context))
-                .reduce((acc, val) => acc + val, 0)
+                .map(scorer => {
+                    const score = scorer.score(context);
+                    console.log(`SumOfChildrenQualifier::${this.action.name}->${scorer.constructor.name}->${score}`);
+                    return score;
+                }).reduce((acc, val) => acc + val, 0)
         );
     }
 }
